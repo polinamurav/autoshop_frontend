0e0fe5cbae2f83de8604a3332d213a6eddfc15ea
Polina Muravieva <polinamur3@gmail.com>
2025-01-30 15:01:08 +0300

    fix styles

--- src/app/views/automobile/automobile-routing.module.ts
+++ src/app/views/automobile/automobile-routing.module.ts
@@ -2,9 +2,11 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import {AutomobilesComponent} from "./automobiles/automobiles.component";
 import {AutomobileAddComponent} from "./automobile-add/automobile-add.component";
+import {AutomobileComponent} from "./automobile/automobile.component";
 
 const routes: Routes = [
   {path: 'automobiles', component: AutomobilesComponent},
+  {path: 'automobiles/:id', component: AutomobileComponent},
   {path: 'automobiles/add', component: AutomobileAddComponent},
 ];
 
--- src/app/views/automobile/automobile/automobile.component.ts
+++ src/app/views/automobile/automobile/automobile.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {AutomobileResponseType} from "../../../../types/automobile-response.type";
+import {AutomobilesService} from "../../../shared/services/automobiles.service";
+import {ActivatedRoute} from "@angular/router";
 
 @Component({
   selector: 'app-automobile',
@@ -7,9 +10,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AutomobileComponent implements OnInit {
 
-  constructor() { }
+  automobile!: AutomobileResponseType;
+
+  constructor(private automobileService: AutomobilesService,
+              private activatedRoute: ActivatedRoute) {
+  }
 
   ngOnInit(): void {
+    this.activatedRoute.params.subscribe(params => {
+      this.automobileService.getAutomobile(params['id'])
+        .subscribe((data: AutomobileResponseType) => {
+          this.automobile = data;
+        });
+    });
   }
 
 }
