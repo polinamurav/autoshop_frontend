4c1f70870d83d4e5efa722028e35449773453da7
Polina Muravieva <polinamur3@gmail.com>
2025-02-04 20:35:54 +0300

    401 error handling

--- src/app/core/auth/auth.interceptor.ts
+++ src/app/core/auth/auth.interceptor.ts
@@ -1,13 +1,19 @@
-import {HttpEvent, HttpHandler, HttpInterceptor, HttpRequest} from "@angular/common/http";
-import {Observable} from "rxjs";
-import {Injectable} from "@angular/core";
-import {AuthService} from "./auth.service";
+import { HttpEvent, HttpHandler, HttpInterceptor, HttpRequest, HttpErrorResponse } from "@angular/common/http";
+import { Injectable } from "@angular/core";
+import { Router } from "@angular/router";
+import { catchError, finalize, Observable, throwError } from "rxjs";
+import { AuthService } from "./auth.service";
+import { LoaderService } from "../../shared/services/loader.service";
+import { MatSnackBar } from "@angular/material/snack-bar";
 
 @Injectable()
 export class AuthInterceptor implements HttpInterceptor {
-
-  constructor(private authService: AuthService) {
-  }
+  constructor(
+    private authService: AuthService,
+    private loaderService: LoaderService,
+    private router: Router,
+    private snackBar: MatSnackBar
+  ) {}
 
   intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
     const tokens = this.authService.getToken();
@@ -19,9 +25,24 @@ export class AuthInterceptor implements HttpInterceptor {
         }
       });
 
-      return next.handle(authReq);
+      return next.handle(authReq).pipe(
+        catchError((error: HttpErrorResponse) => this.handleError(error)),
+        finalize(() => this.loaderService.hide())
+      );
     }
 
-    return next.handle(req);
+    return next.handle(req).pipe(
+      catchError((error: HttpErrorResponse) => this.handleError(error)),
+      finalize(() => this.loaderService.hide())
+    );
+  }
+
+  private handleError(error: HttpErrorResponse): Observable<never> {
+    if (error.status === 401) {
+      this.authService.removeToken();
+      this.snackBar.open("Сессия истекла. Авторизуйтесь заново");
+      this.router.navigate(['/login']);
+    }
+    return throwError(() => error);
   }
 }
