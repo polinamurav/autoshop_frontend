a9c6c6b9e895249f3422370b76bc5c59c882bf0b
Polina Muravieva <polinamur3@gmail.com>
2025-02-04 17:58:36 +0300

    add guard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,6 +2,8 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import {LayoutComponent} from "./shared/layout/layout.component";
 import {MainComponent} from "./views/main/main.component";
+import {AuthGuard} from "./core/auth/auth.guard";
+import {AuthForwardGuard} from "./core/auth/auth-forward.guard";
 
 const routes: Routes = [
   {
@@ -9,12 +11,12 @@ const routes: Routes = [
     component: LayoutComponent,
     children: [
       {path: '', component: MainComponent},
-      {path: '', loadChildren: () => import('./views/user/user.module').then(m => m.UserModule)},
+      {path: '', loadChildren: () => import('./views/user/user.module').then(m => m.UserModule), canActivate: [AuthForwardGuard]},
       {path: '', loadChildren: () => import('./views/automobile/automobile.module').then(m => m.AutomobileModule)},
-      {path: '', loadChildren: () => import('./views/models/models.module').then(m => m.ModelsModule)},
-      {path: '', loadChildren: () => import('./views/application/application.module').then(m => m.ApplicationModule)},
-      {path: '', loadChildren: () => import('./views/account/account.module').then(m => m.AccountModule)},
-      {path: '', loadChildren: () => import('./views/stat/stat.module').then(m => m.StatModule)},
+      {path: '', loadChildren: () => import('./views/models/models.module').then(m => m.ModelsModule), canActivate: [AuthGuard]},
+      {path: '', loadChildren: () => import('./views/application/application.module').then(m => m.ApplicationModule), canActivate: [AuthGuard]},
+      {path: '', loadChildren: () => import('./views/account/account.module').then(m => m.AccountModule), canActivate: [AuthGuard]},
+      {path: '', loadChildren: () => import('./views/stat/stat.module').then(m => m.StatModule), canActivate: [AuthGuard]},
     ]
   }
 ];
